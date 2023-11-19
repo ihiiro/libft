5d5977a6b629d3a48c65fa7dbbbe707d8f140f59
Yassir El yaqine <yel-yaqi@e2r12p3.1337.ma>
2023-11-19 15:20:19 +0100

    ft_memset.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 16:37:17 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2023/11/19 15:02:49 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2023/11/19 15:10:09 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,6 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
+void	*ft_memset(void *b, int c, size_t len);
 
 #endif
