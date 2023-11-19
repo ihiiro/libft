946e07dcaa06d16214ccbebf4ca253d76d0079b8
Yassir El yaqine <yel-yaqi@e2r12p3.1337.ma>
2023-11-19 12:44:44 +0100

    ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,30 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/18 16:37:17 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2023/11/19 10:21:02 by yel-yaqi         ###   ########.fr       */
+/*   Created: 2023/11/19 10:21:51 by yel-yaqi          #+#    #+#             */
+/*   Updated: 2023/11/19 12:23:20 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stddef.h>
-# include <stdio.h>
-# include <string.h>
-# include <assert.h>
+#include "libft.h"
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t	i;
 
-#endif
+	i = 0;
+	while (*(s + i))
+		i++;
+	while (i >= 1)
+	{
+		if (*(s + i) == (char)c)
+			return ((char *)s + i);
+		i--;
+	}
+	if (*(s + i) == (char)c)
+		return ((char *)s + i);
+	return (NULL);
+}
