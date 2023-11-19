3419e2b10dff7af19106cdd0c792329c30f16343
Yassir El yaqine <yel-yaqi@e2r12p3.1337.ma>
2023-11-19 14:20:40 +0100

    ft_memchr.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 16:37:17 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2023/11/19 12:45:47 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2023/11/19 14:18:56 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,6 @@ int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif
